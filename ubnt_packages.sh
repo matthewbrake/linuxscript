7a3349a107431fae8b1dfd26d42cbf76eb696f2f
matthewbrake <43625592+matthewbrake@users.noreply.github.com>
2024-03-25 13:03:21 -0400

    Update ubnt_packages.sh

--- ubnt_packages.sh
+++ ubnt_packages.sh
@@ -6,6 +6,10 @@ echo "Updating package index and upgrading system..."
 sudo apt update
 sudo apt upgrade -y
 
+# Depencies 
+sudo apt install -y git build-essential apt-utils cmake libfontconfig1 libglu1-mesa-dev libgtest-dev libspdlog-dev libboost-all-dev libncurses5-dev libgdbm-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev libbz2-dev mesa-common-dev qtbase5-dev qtchooser qt5-qmake qtbase5-dev-tools libqt5websockets5 libqt5websockets5-dev qtdeclarative5-dev golang-go qtbase5-dev libqt5websockets5-dev libspdlog-dev python3-dev libboost-all-dev mingw-w64 nasm
+echo "Installing dependicences..."
+
 # Utilities (50 packages)
 echo "Installing utilities..."
 sudo apt install -y \
