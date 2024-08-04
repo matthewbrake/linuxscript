3cf62cb9ef59176b66fbeb941f105a8ac2fa21cf
matthewbrake <43625592+matthewbrake@users.noreply.github.com>
2024-08-04 01:32:37 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -128,3 +128,28 @@ wget -qO- https://get.casaos.io | sudo bash
 
 # WebAdmin
 sudo curl -fsSL -o ~/setup-repos.sh https://raw.githubusercontent.com/webmin/webmin/master/setup-repos.sh && sudo sh ~/setup-repos.sh && apt-get install webmin --install-recommends
+
+
+# Oled
+sudo raspi-config nonint do_i2c 0
+
+sudo apt update
+sudo apt-get install -y python3-dev python3-pip i2c-tools git build-essential libi2c-dev lm-sensors
+
+# Clone Luma and install
+mkdir /home/user/drivers 
+cd /home/user/drivers
+git clone https://github.com/rm-hull/luma.oled.git
+cd luma.oled
+sudo python3 -m pip install -e .
+cd ..
+
+# Install psutil (for system info)
+sudo python3 -m pip install psutil
+
+# Clone luma.examples Repository (optional, for testing)
+cd ~
+git clone https://github.com/rm-hull/luma.examples.git /home/user/drivers
+cd /home/user/drivers/luma.examples/examples
+sudo python3 setup.py install
+
