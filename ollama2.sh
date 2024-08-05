183248303ed6f28db8b42a6d4c5cdd0c9ab2910d
matthewbrake <43625592+matthewbrake@users.noreply.github.com>
2024-08-05 10:08:19 -0400

    Update ollama2.sh

--- ollama2.sh
+++ ollama2.sh
@@ -1,32 +1,17 @@
 #!/bin/bash
+MODEL_TO_RUN="llama3"
 
-# Install Ollama and model if not already installed
-if ! command -v ollama &> /dev/null; then
-  echo "Installing Ollama..."
-  curl -fsSL https://ollama.com/install.sh | sh
+echo "Installing Ollama..."
+curl -fsSL https://ollama.com/install.sh | sh
 
-  # Set OLLAMA_HOST environment variable
-  export OLLAMA_HOST=0.0.0.0
-fi
+# Set OLLAMA_HOST environment variable
+export OLLAMA_HOST=0.0.0.0
+export OLLAMA_KEEP_ALIVE=24hr
+echo "Installing model $MODEL_TO_RUN..."
+ollama pull "$MODEL_TO_RUN"
 
-if ! ollama list | grep -q "$MODEL_TO_RUN"; then
-  echo "Installing model $MODEL_TO_RUN..."
-  ollama pull "$MODEL_TO_RUN"
-fi
-
-# Signal handler for graceful shutdown
-shutdown_handler() {
-  echo "Stopping Ollama server..."
-  ollama stop
-  exit 0
-}
-
-trap shutdown_handler SIGINT SIGTERM
-
-# Start Ollama server in the background if not already running
-if ! ps aux | grep "[o]llama serve" &> /dev/null; then
-  (ollama serve > ollama.log 2>&1) &
-fi
+# Start Ollama server in the background 
+(ollama serve > ollama.log 2>&1) &
 
 echo "Ollama service is running!"
 echo "Running model $MODEL_TO_RUN..."
