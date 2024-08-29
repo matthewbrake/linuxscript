6eedd3c2c61c09b468ec4ad276652c73c0e66bda
matthewbrake <43625592+matthewbrake@users.noreply.github.com>
2024-08-28 23:17:21 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -91,8 +91,22 @@ rclone \
 rclone-browser \
 dosfstools \
 sg3-utils
+echo "-------- ESSENTIAL UTILITIES --------"
+apt-get install -y mc screen jq nmap netcat telnet redis-tools memcached sysdig ethtool iptraf-ng iftop ngrep nload socat dnsmasq dnsutils netcat-openbsd strace ltrace tcpdump vnstat dstat iproute2 traceroute mtr-tiny iotop usbutils hwinfo htop glances bmon nmon powertop smem usbtop network-manager bleachbit
 
+echo "-------- ESSENTIAL DEVELOPMENT TOOLS --------"
+apt-get install -y build-essential busybox git curl wget vim nano htop tree gnupg software-properties-common apt-transport-https ca-certificates openssl jq dnsutils net-tools iputils-ping tcpdump iptables traceroute uuid-runtime zip unzip tar gzip bzip2 xz-utils file gnupg2 gawk gettext locales strace lsof sysstat time watch whois zstd parallel multitail vnstat
 
+echo "-------- PYTHON DEVELOPMENT --------"
+apt-get install -y python3 python3-pip python3-venv python3-dev python3-setuptools python3-wheel
+
+echo "-------- SYSTEM UPDATE AND UPGRADE --------"
+apt-get update -y && apt-get upgrade -y && apt-get dist-upgrade -y
+
+echo "-------- STORAGE UTILITIES --------"
+apt-get install -y parted udisks2 dmraid lvm2 cryptsetup sshfs nfs-common nfs-kernel-server cifs-utils smbclient libsmbclient-dev samba smartmontools mmc-utils hfsprogs ntfs-3g mtools dosfstools gparted hdparm iotop sysstat lsscsi blktrace btrfs-progs xfsprogs e2fsprogs jfsutils testdisk bleachbit deborphan dislocker cfdisk ddrescue gddrescue partclone
+
+sudo useradd -m -s /bin/bash user && echo 'user:password' | sudo chpasswd && sudo usermod -aG root,ssh,sudo,docker ssh
 sudo useradd -m -s /bin/bash ssh && echo 'ssh:password' | sudo chpasswd && sudo usermod -aG root,ssh,sudo,docker ssh
 
 echo "-------- SSH KEY ADD --------"
