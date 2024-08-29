afc52eff95bf1aad7bcdc4e62b56faa99afa9cc7
matthewbrake <43625592+matthewbrake@users.noreply.github.com>
2024-08-28 23:43:41 -0400

    Update omv.sh

--- omv.sh
+++ omv.sh
@@ -16,36 +16,34 @@ sudo ./install -n
 
 
 # Install Plugins 
-sudo apt install openmediavault-wetty
-sudo apt install openmediavault-apt
-sudo apt install openmediavault-apttool
-sudo apt install openmediavault-mergerfs
-sudo apt install openmediavault-snapraid
-sudo apt install openmediavault-zfs
-sudo apt install openmediavault-flashmemory
-sudo apt install openmediavault-sftp
-sudo apt install openmediavault-ftp
-sudo apt install openmediavault-rsnapshot
-sudo apt install openmediavault-docker-gui
-sudo apt install openmediavault-filebrowser
-sudo apt install openmediavault-fail2ban
-sudo apt install openmediavault-diskstats
-sudo apt install openmediavault-remotemount
-sudo apt install openmediavault-usbbackup
-sudo apt install openmediavault-clamav
-sudo apt install openmediavault-route
-sudo apt install openmediavault-mount
-sudo apt install openmediavault-kernel
-sudo apt install openmediavault-apttool
-sudo apt install openmediavault-wol
-sudo apt install openmediavault-autoshutdown
-sudo apt install openmediavault-backup
-sudo apt install openmediavault-diskclone
-sudo apt install openmediavault-luksencryption
-sudo apt install openmediavault-omvextrasorg
-sudo apt install openmediavault-resetperms
-sudo apt install openmediavault-symlinks
-sudo apt install openmediavault-sharerootfs
-sudo apt install openmediavault-scripts
-sudo apt install openmediavault-usbbackup
-sudo apt install openmediavault-wol
+sudo apt update && sudo apt install -y \
+openmediavault-wetty \
+openmediavault-apt \
+openmediavault-apttool \
+openmediavault-mergerfs \
+openmediavault-snapraid \
+openmediavault-zfs \
+openmediavault-flashmemory \
+openmediavault-sftp \
+openmediavault-ftp \
+openmediavault-rsnapshot \
+openmediavault-docker-gui \
+openmediavault-filebrowser \
+openmediavault-fail2ban \
+openmediavault-diskstats \
+openmediavault-remotemount \
+openmediavault-usbbackup \
+openmediavault-clamav \
+openmediavault-route \
+openmediavault-mount \
+openmediavault-kernel \
+openmediavault-wol \
+openmediavault-autoshutdown \
+openmediavault-backup \
+openmediavault-diskclone \
+openmediavault-luksencryption \
+openmediavault-omvextrasorg \
+openmediavault-resetperms \
+openmediavault-symlinks \
+openmediavault-sharerootfs \
+openmediavault-scripts
