d5c5627bb487d3f8e9049f95a54f7ce4d6ae3768
matthewbrake <43625592+matthewbrake@users.noreply.github.com>
2024-08-28 23:11:13 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -101,9 +101,16 @@ mkdir -p /home/user/.ssh && touch /home/user/.ssh/authorized_keys && echo "ssh-r
 sudo sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config && sudo systemctl restart sshd
 
 # Directories
+newdir="/mnt/FS-OMV" && sudo mkdir -p "$newdir" && sudo chown -R 1000:1000 "$newdir" && sudo chmod -R 755 "$newdir" && ls -la "$newdir"
+newdir="/mnt/sftp/sonicbit" && sudo mkdir -p "$newdir" && sudo chown -R 1000:1000 "$newdir" && sudo chmod -R 755 "$newdir" && ls -la "$newdir"
+newdir="/mnt/gdrive-mbrake1994" && sudo mkdir -p "$newdir" && sudo chown -R 1000:1000 "$newdir" && sudo chmod -R 755 "$newdir" && ls -la "$newdir"
 newdir="/mnt/zima" && sudo mkdir -p "$newdir" && sudo chown -R 1000:1000 "$newdir" && sudo chmod -R 755 "$newdir" && ls -la "$newdir"
+newdir="/mnt/zima/HDD2" && sudo mkdir -p "$newdir" && sudo chown -R 1000:1000 "$newdir" && sudo chmod -R 755 "$newdir" && ls -la "$newdir"
+newdir="/mnt/zima/HDD3" && sudo mkdir -p "$newdir" && sudo chown -R 1000:1000 "$newdir" && sudo chmod -R 755 "$newdir" && ls -la "$newdir"
 newdir="/docker" && sudo mkdir -p "$newdir" && sudo chown -R 1000:1000 "$newdir" && sudo chmod -R 755 "$newdir" && ls -la "$newdir"
+newdir="/HDD" && sudo mkdir -p "$newdir" && sudo chown -R 1000:1000 "$newdir" && sudo chmod -R 755 "$newdir" && ls -la "$newdir"
 newdir="/HDD2" && sudo mkdir -p "$newdir" && sudo chown -R 1000:1000 "$newdir" && sudo chmod -R 755 "$newdir" && ls -la "$newdir"
+newdir="/HDD3" && sudo mkdir -p "$newdir" && sudo chown -R 1000:1000 "$newdir" && sudo chmod -R 755 "$newdir" && ls -la "$newdir"
 
 newhostname="PI-SERVER"; echo $newhostname > /etc/hostname && echo "127.0.0.1 $newhostname localhost" > /etc/hosts && echo "::1  localhost ip6-localhost ip6-loopback" >> /etc/hosts
 newhostname="PI-SERVER"; ip=$(hostname -I | awk '{print $1}'); echo $newhostname > /etc/hostname && echo "127.0.0.1 $newhostname localhost" > /etc/hosts && echo "::1  localhost ip6-localhost ip6-loopback" >> /etc/hosts && echo "$ip $newhostname" >> /etc/hosts
@@ -115,42 +122,4 @@ bash -c "$(curl -fsSL https://get.docker.com)"
 sudo systemctl enable docker.service && sudo systemctl enable containerd.service
 sudo usermod -aG docker user
 
-# Open Media Vault
-sudo apt-get update
-sudo apt-get upgrade -y
-wget -O - https://github.com/OpenMediaVault-Plugin-Developers/installScript/raw/master/preinstall | sudo bash
-
-wget https://github.com/OpenMediaVault-Plugin-Developers/installScript/raw/master/install
-chmod +x install
-sudo ./install -n
-
-# Casa OS Install 
-wget -qO- https://get.casaos.io | sudo bash
-
-# WebAdmin
-sudo curl -fsSL -o ~/setup-repos.sh https://raw.githubusercontent.com/webmin/webmin/master/setup-repos.sh && sudo sh ~/setup-repos.sh && apt-get install webmin --install-recommends
-
-
-# Oled
-sudo raspi-config nonint do_i2c 0
-
-sudo apt update
-sudo apt-get install -y python3-dev python3-pip i2c-tools git build-essential libi2c-dev lm-sensors
-
-# Clone Luma and install
-mkdir /home/user/drivers 
-cd /home/user/drivers
-git clone https://github.com/rm-hull/luma.oled.git
-cd luma.oled
-sudo python3 -m pip install -e .
-cd ..
-
-# Install psutil (for system info)
-sudo python3 -m pip install psutil
-
-# Clone luma.examples Repository (optional, for testing)
-cd ~
-git clone https://github.com/rm-hull/luma.examples.git /home/user/drivers
-cd /home/user/drivers/luma.examples/examples
-sudo python3 setup.py install
 
