08bdefb02962b512a9b9e434f0f83a65e2180826
matthewbrake <43625592+matthewbrake@users.noreply.github.com>
2024-09-28 18:03:55 -0400

    Update fstab_mounts.sh

--- fstab_mounts.sh
+++ fstab_mounts.sh
@@ -12,7 +12,7 @@ add_to_fstab() {
 
 # Add mount entries
 add_to_fstab "//10.0.0.53/fs-omv /mnt/fs-omv cifs username=user,password=password,uid=1000,gid=1000,nofail 0 0"
-add_to_fstab "//10.0.0.150/HDD2 /mnt/zima/HDD2 cifs username=user,password=password,uid=1000,gid=1000,nofail 0 0"
+add_to_fstab "//10.0.0.150/HDD2 /mnt/zima/HDD cifs username=user,password=password,uid=1000,gid=1000,nofail 0 0"
 add_to_fstab "//10.0.0.150/HDD3 /mnt/zima/HDD3 cifs username=user,password=password,uid=1000,gid=1000,nofail 0 0"
 add_to_fstab "//10.0.0.150/zima_sftp_sonicbit /mnt/zima/sonicbit-sftp cifs username=user,password=password,uid=1000,gid=1000,nofail 0 0"
 add_to_fstab "//10.0.0.150/zima_gdrive_mbrake1994 /mnt/zima/gdrive-mbrake1994 cifs username=user,password=password,uid=1000,gid=1000,nofail 0 0"
