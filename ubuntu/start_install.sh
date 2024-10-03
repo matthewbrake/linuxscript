949062e5a153f18fa4850f8198d39dffc668a23e
matthewbrake <43625592+matthewbrake@users.noreply.github.com>
2024-10-03 15:23:31 -0400

    Update start_install.sh

--- ubuntu/start_install.sh
+++ ubuntu/start_install.sh
@@ -1,6 +1,10 @@
 
 # DOCKER DESKTOP
 sudo apt update && sudo apt install -y software-properties-common && curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker.gpg && echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null && sudo apt update && sudo apt install -y docker-desktop
+# APT FAST
+sudo add-apt-repository ppa:apt-fast/stable
+sudo apt-get update
+sudo apt-get -y install apt-fast
 
 # VLC / KVM PLAYER
 apt install vlc
