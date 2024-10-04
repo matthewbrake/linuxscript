041b5093f0b0187b2c3fc094186604e5870f20af
matthewbrake <43625592+matthewbrake@users.noreply.github.com>
2024-10-04 09:28:41 -0400

    Update start_install.sh

--- ubuntu/start_install.sh
+++ ubuntu/start_install.sh
@@ -1,4 +1,16 @@
-
+sudo apt purge \
+  cinnamon* \
+  gnome-* \
+  mate-* \
+  xfce4* \
+  light-locker* \
+  lightdm* \
+  papirus-icon-theme \
+  yaru-theme-icon \
+  breeze* \
+  greybird-gtk-theme \
+  *-icon-theme \
+  fonts-*
 # DOCKER DESKTOP
 sudo apt update && sudo apt install -y software-properties-common && curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker.gpg && echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null && sudo apt update && sudo apt install -y docker-desktop
 # APT FAST
