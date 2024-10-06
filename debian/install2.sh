9c0905957332b4202e561b4d272ad093ac930ff6
matthewbrake <43625592+matthewbrake@users.noreply.github.com>
2024-10-06 11:34:37 -0400

    Update install2.sh

--- debian/install2.sh
+++ debian/install2.sh
@@ -23,3 +23,34 @@ E: Package 'exfat-utils' has no installation candidate
 E: Package 'zfsutils-linux' has no installation candidate
 E: Unable to locate package iscsi-initiator-utils
 root@cubic:~# 
+
+echo "deb http://deb.debian.org/debian bookworm main contrib non-free non-free-firmware" | sudo tee -a /etc/apt/sources.list
+echo "deb http://deb.debian.org/debian-security/ bookworm-security main contrib non-free non-free-firmware" | sudo tee -a /etc/apt/sources.list
+echo "deb http://deb.debian.org/debian bookworm-updates main contrib non-free non-free-firmware" | sudo tee -a /etc/apt/sources.list
+
+sudo apt install -y \
+    gparted gnome-disk-utility baobab ncdu duf fdisk gdisk parted smartmontools hdparm fio iotop \
+    iftop nmon htop atop sysstat ioping bonnie++ gddrescue testdisk extundelete foremost rclone rsync \
+    grsync syncthing duplicity timeshift backintime-qt clonezilla fsarchiver partclone partimage p7zip-full \
+    zip unzip ark file-roller fdupes rdfind bleachbit secure-delete wipe cryptsetup ecryptfs-utils \
+    encfs smbclient cifs-utils samba nfs-common sg3-utils scsitools lsscsi sdparm nvme-cli mdadm lvm2 \
+    thin-provisioning-tools xfsprogs btrfs-progs e2fsprogs dosfstools mtools ntfs-3g exfatprogs udftools \
+    squashfs-tools f2fs-tools nbd-client open-iscsi targetcli-fb util-linux coreutils \
+    dmraid dmsetup multipath-tools bcache-tools zfsutils-linux zfs-fuse \
+    reiserfsprogs jfsutils hfsplus hfsprogs nilfs-tools xfsdump \
+    fatresize fatsort exfat-fuse exfat-utils \
+    nwipe secure-delete srm wipe \
+    ddrescue photorec sleuthkit autopsy \
+    dar dar-static kdar \
+    fuse sshfs curlftpfs bindfs archivemount \
+    aufs-tools overlayfs-tools \
+    parted gpart gptfdisk \
+    quota quotatool \
+    acl attr eiciel \
+    inotify-tools incron \
+    logrotate \
+    tmux screen \
+    iotop blktrace blkid lsblk \
+    hwinfo lshw inxi \
+    stress stress-ng fio \
+    ethtool
