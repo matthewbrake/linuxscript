0c62a2870b61c9b25212d089efeed842800a994c
matthewbrake <43625592+matthewbrake@users.noreply.github.com>
2024-10-06 11:31:54 -0400

    Create install2.sh

---
+++ debian/install2.sh
@@ -0,0 +1,25 @@
+This may mean that the package is missing, has been obsoleted, or
+is only available from another source
+
+Package zfsutils-linux is not available, but is referred to by another package.
+This may mean that the package is missing, has been obsoleted, or
+is only available from another source
+
+Package exfat-utils is not available, but is referred to by another package.
+This may mean that the package is missing, has been obsoleted, or
+is only available from another source
+
+E: Unable to locate package df
+E: Unable to locate package du
+E: Unable to locate package lsblk
+E: Unable to locate package blkid
+E: Unable to locate package ddrescue
+E: Unable to locate package photorec
+E: Package 'rar' has no installation candidate
+E: Package 'unrar' has no installation candidate
+E: Unable to locate package shred
+E: Unable to locate package veracrypt
+E: Package 'exfat-utils' has no installation candidate
+E: Package 'zfsutils-linux' has no installation candidate
+E: Unable to locate package iscsi-initiator-utils
+root@cubic:~# 
