9d9a8a7396926e147cb16da6a07be654e8f7b00f
matthewbrake <43625592+matthewbrake@users.noreply.github.com>
2024-10-16 23:46:35 -0400

    Update start_install.sh

--- ubuntu/start_install.sh
+++ ubuntu/start_install.sh
@@ -1,4 +1,6 @@
- 
+# FLATPACK
+
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrep
 # DOCKER DESKTOP
 sudo apt update && sudo apt install -y software-properties-common && curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker.gpg && echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null && sudo apt update && sudo apt install -y docker-desktop
 # APT FAST
